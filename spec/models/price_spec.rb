15730d8a834b64cc2986ae0f26a8c748029929c9
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-02 17:28:24 -0700

    wrote first spec

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can hit the coinbase api and #get_buy/sell price" do
+    price = create(:price)
+
+    expect(price.get_sell_price.class).to be_a(Float)
+    expect(price.get_buy_price.class).to be_a(Float)
+  end
 end
