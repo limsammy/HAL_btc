8db6cd985d33e6738b37cec2dff2b1f6ef3a9b2a
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-02 18:21:49 -0700

    Installed figaro for api tokens

--- app/models/price_btc.rb
+++ app/models/price_btc.rb
@@ -1,4 +1,11 @@
+require 'coinbase/wallet'
+
 class PriceBtc < ApplicationRecord
+  def initialize
+    client = Coinbase::Wallet::Client.new(api_key: ENV["COINBASE_KEY"],
+      api_secret: ENV[COINBASE_SECRET])
+  end
+
   def self.get_buy_price
     
   end
